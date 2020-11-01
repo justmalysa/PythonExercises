246c0d3a95c96e7c8bf00bf069d3711ee0b165c2
justmalysa <justynamalysa98@gmail.com>
2020-11-01 22:08:50 +0100

    Add wejscie_wyjscie directory

---
+++ wejscie_wyjscie/wprowadzanie_danych.py
@@ -0,0 +1,7 @@
+print("Podaj imie")
+name = input()
+print("Podaj naziwsko")
+surname = input()
+print("Podaj rok urodzenia")
+year = input()
+print(name + ', '+ surname + ', ' + year)
---
+++ wejscie_wyjscie/zapis_danych.py
@@ -0,0 +1,8 @@
+print("Podaj kod")
+code1 = input()
+print("Podaj ponownie kod")
+code2 = input()
+if(code1==code2):
+    print("Twoj kod jest zgodny")
+else:
+    print("Twoj kod jest bledny")
